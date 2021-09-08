f3130dc7d52e7d281543ad61a612bd36b94a2b66
Mark Sinakaev <high-school@bk.ru>
2021-09-08 13:10:46 +0300

    volatile ready

---
+++ src/com/company/Volatile.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Volatile {
+
+    static int i;
+
+    public static void main(String[] args) throws InterruptedException {
+        new MyThreadWrite();
+        new MyThreadRead();
+    }
+
+    static class MyThreadWrite extends Thread{
+
+        @Override
+        public void run() {
+            while(i < 5){
+                System.out.println("increment i to " + (++i));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public MyThreadWrite(){
+            this.start();
+        }
+    }
+
+    static class MyThreadRead extends Thread {
+
+        @Override
+        public void run() {
+            int localVar = i;
+            while(localVar < 5){
+                if(localVar != i){
+                    System.out.println("new value of i is " + i);
+                    localVar = i;
+                }
+            }
+        }
+
+        public MyThreadRead(){
+            this.start();
+        }
+    }
+}
+
+
+
